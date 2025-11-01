b764af6e20167e0fbbee7faf963c1d3561992b54
swaleha2002 <mullaswaleha402@gmail.com>
2025-11-01 16:06:15 +0530

    Model Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pandas as pd
 
+from src.components.model_trainer import ModelTrainer
 from src.exception import CustomException
 from src.logger import logging
 
@@ -55,4 +56,9 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    
+    modeltrainer = ModelTrainer()
+    best_model = modeltrainer.initiate_model_trainer(train_arr,test_arr)
+    print(best_model)
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import dill
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
 
@@ -12,5 +13,26 @@ def save_object(file_path,obj):
         with open(file_path,'wb') as file:
             pickle.dump(obj,file)
             logging.info(f"Object saved at {file_path}")
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train the model
+            model.fit(X_train,y_train)
+            
+            # Predicting the test data
+            y_test_pred = model.predict(X_test)
+            
+            # Get the r2 score for the model
+            test_model_score = r2_score(y_test,y_test_pred)
+            
+            report[list(models.keys())[i]] = test_model_score
+            
+        return report
     except Exception as e:
         raise CustomException(e,sys)
